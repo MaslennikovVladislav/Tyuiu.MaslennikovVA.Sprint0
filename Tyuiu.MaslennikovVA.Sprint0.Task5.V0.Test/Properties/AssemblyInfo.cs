772f6d95a070fdcfda0e2d3a45f7fec3d3dc4dac
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-24 11:08:24 +0500

    Task5

---
+++ Tyuiu.MaslennikovVA.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MaslennikovVA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MaslennikovVA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("05abc268-2b50-441a-b48b-ca32ba93eea4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
